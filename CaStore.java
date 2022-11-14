e5b1e1f488b9fc702de997aabae4d8421776635a
Sabarna Sarkar <64100393+Sabarna-dotcom@users.noreply.github.com>
2022-11-14 18:36:15 +0530

    Commit
    Commit

--- CaStore.java
+++ CaStore.java
@@ -1,3 +1,4 @@
+package Selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,11 +10,13 @@ public class CaStore {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "E:\\study-meterial\\myStudy\\Language\\Java\\Selenium\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\X022654\\\\OneDrive - Thomson Reuters Incorporated\\\\Desktop\\\\chrome_driver\\\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		String Search_key="family law";
 		driver.manage().window().maximize();
-		driver.get("https://store.thomsonreuters.ca/en-ca/home");
+		driver.get("https://ams_uat:AMS_65uat@store-tr-ca-uat-ams.ewp.thomsonreuters.com/en-ca/home");
+		//driver.get("https://store.thomsonreuters.ca/en-ca/home");
+		//driver.get("https://store.thomsonreuters.ca/fr-ca/home");
 		Thread.sleep(500);
 		driver.findElement(By.xpath("//span[@class='tr-Button-body']")).click();
 		Thread.sleep(5000);
@@ -24,11 +27,13 @@ public class CaStore {
 		Thread.sleep(5000);
 		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='tr-coveo-results-highlight']/span")).getText(),Search_key);
 		String book2=add_to_cart(driver);
-		driver.navigate().to("https://store.thomsonreuters.ca/en-ca/cart");
+		driver.navigate().to("https://ams_uat:AMS_65uat@store-tr-ca-uat-ams.ewp.thomsonreuters.com/en-ca/cart");
+		//driver.navigate().to("https://store.thomsonreuters.ca/en-ca/cart");
+		//driver.navigate().to("https://store.thomsonreiters.ca/fr-ca/cart");
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("(//span[@class='tr-Svg-inner'])[14]")).click();
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@id='promoCodeInput']")).sendKeys("10OFF");
+		driver.findElement(By.xpath("//input[@id='promoCodeInput']")).sendKeys("1OFF");
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//button[@class='tr-Anchor tr-Button tr-Button--primary']")).click();
 		Thread.sleep(3000);
@@ -52,9 +57,34 @@ public class CaStore {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@id='phoneNumber']")).sendKeys("9090909090");
 		Thread.sleep(3000);
-		WebElement radio1 = driver.findElement(By.xpath("(//input[@id='freeShipping'])[1]"));
-		radio1.click();
-		Thread.sleep(5000);
+		driver.findElement(By.xpath("//strong[normalize-space()='Free standard shipping']")).click();
+		driver.findElement(By.xpath("//button[@class='tr-Anchor tr-Button tr-Button--primary tr-Button--semiCircular']")).click();
+		Thread.sleep(3000);
+		/*driver.findElement(By.xpath("//label[@for='isSameAsShipping']//span[@class='tr-FormCheckboxControl-labelText']")).click();
+		Thread.sleep(500);
+		driver.findElement(By.xpath("(//input[@id='searchInput-9'])[1]")).sendKeys("can");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//span[normalize-space()=\"Canada, ON, Vaughan, Maple, Canada's wonderland dr\"])[1]")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='billingPostalCode']")).sendKeys("");
+		driver.findElement(By.xpath("//input[@id='billingPostalCode']")).sendKeys("A1A 1A1");*/
+		driver.switchTo().frame("CREDIT_CARD_IFRAME_ID");
+		driver.findElement(By.xpath("//*[contains(text(),'Name on card')]//following::input[1]")).sendKeys("Test");
+		driver.findElement(By.xpath("//*[contains(text(),'Card number')]//following::input[1]")).sendKeys("4114360123456785");
+		WebElement month = driver.findElement(By.id("selectCardMonth"));
+		Select month_card = new Select(month);
+		month_card.selectByIndex(5);
+		WebElement year = driver.findElement(By.id("selectCardYear"));
+		Select year_card = new Select(year);
+		year_card.selectByIndex(6);
+		driver.findElement(By.id("SecurityCode")).sendKeys("123");
+		driver.switchTo().defaultContent();
+		driver.findElement(By.xpath("//span[normalize-space()='I authorize payment']")).click();
+		driver.findElement(By.xpath("//button[contains(@data-testid,'checkout_btn')]")).click();
+		Thread.sleep(20000);
+		driver.findElement(By.xpath("(//button[@type='button'])[1]")).click();
+		Thread.sleep(20000);
+		print(driver.findElement(By.xpath("//span[@class='tr-OrderConfirmation-normalText tr-Typography tr-Typography--xxs tr-Typography--left']")).getText());
 		driver.close();
 		print("Success");
 
